3e04cef62afc512f78c06089bf643a86fdb244f0
Tom Pawlak <tompwk@gmail.com>
2014-02-07 22:48:04 +0000

    TemplateHelper updates

--- core/server/helpers/templatehelper.js
+++ core/server/helpers/templatehelper.js
@@ -1,8 +1,9 @@
 var _s      = require('underscore.string'),
     format  = require('util').format,
-    moment  = require('moment');
+    moment  = require('moment')
+    cfg     = require('nconf');
 
-function TemplateHelper () {}
+var TemplateHelper = module.exports = function () {};
 
 TemplateHelper.prototype.url = function (post) {
     return format(
@@ -11,12 +12,14 @@ TemplateHelper.prototype.url = function (post) {
         _s.lpad(post.scheduled.getMonth() + 1, 2, '0'), 
         post.slug
     );
-}
+};
 
 TemplateHelper.prototype.date = function (post, format) {
     return post.scheduled
         ? moment(post.scheduled).format(format)
         : '';
-}
+};
 
-module.exports = new TemplateHelper();
+module.exports.init = function (app) {
+    app.locals.helper = new TemplateHelper(); 
+};
