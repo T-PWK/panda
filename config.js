bd3431b676482ab03822d6756df7073e3223269e
Tom Pawlak <tompwk@gmail.com>
2014-02-07 22:47:08 +0000

    Configuration changes

--- config.js
+++ config.js
@@ -2,20 +2,8 @@
 
 var config = module.exports = {
     // ### Environment ###
-    development: {
-        url: 'http://127.0.0.1',
-        database: {
-            type: 'memory'
-            // connection: {
-            //     uri: process.env.MONGOLAB_URI
-            // }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '3000'
-        }
-    },
     production: {
+        theme: 'casper',
         url: 'http://panda-blog.herokuapp.com/',
         database: {
             type: 'mongo',
--- core/server/config/index.js
+++ core/server/config/index.js
@@ -1,25 +1,38 @@
 var when    = require('when'),
     cfg     = require('nconf'),
-    config  = require('../../../config');
+    config  = require('../../../config'),
+    path    = require('path').join;
 
 function init () {
-    cfg
-        .env({ separator: '__' })
-        .add('memory')
-        .add('userconf', { type: 'literal', store: config })
-        .defaults({
-            NODE_ENV: 'development',
-            PORT: 3000
-        });
 
-    var env = cfg.get('NODE_ENV');
+    // Make sure NODE_ENV is always setup as it is used by express
+    var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
+        defaultTheme = 'casper'
+        basePath = path(__dirname, '../../..'),
+        themeBasePath = path(basePath, 'content/themes');
+
+    cfg.env({ separator: '___' }).add('memory');
+
+    if (config[env]) {
+        cfg.add('userconf', { type: 'literal', store: config[env] });
+    }
+
+    cfg.defaults({
+        theme: {
+            name: 'casper', 
+            viewPath: path(themeBasePath, 'casper'),
+            staticPath: path(themeBasePath, defaultTheme, 'assets')
+        },
+        url: 'http://127.0.0.1',
+        server: { host: '127.0.0.1', port: 3000 },
+        database: { type: 'memory' }
+    });
 
     cfg.set('env', env);
     cfg.set('is:development', 'development' === env);
     cfg.set('is:production', 'production' === env);
 
-    // Make sure NODE_ENV is always setup as it is used by express
-    process.env.NODE_ENV = env;
+    // console.log(require('util').inspect(cfg, {depth: 5}))
 
     return when();
 }
