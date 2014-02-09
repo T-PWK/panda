a398b6322f79c9f1fb4f943eb2cb7f8a7dd396a4
Tom Pawlak <tompwk@gmail.com>
2014-02-09 01:05:19 +0000

    Configuration updates

--- core/server/config/index.js
+++ core/server/config/index.js
@@ -1,38 +1,50 @@
-var when        = require('when'),
-    cfg         = require('nconf'),
-    config      = require('../../../config'),
-    path        = require('path'),
-    appRoot     = path.resolve(__dirname, '../../..'),
-    themesRoot  = path.resolve(appRoot, 'content/themes');
+var when            = require('when'),
+    cfg             = require('nconf'),
+    config          = require('../../../config'),
+    path            = require('path'),
+    appRoot         = path.resolve(__dirname, '../../..'),
+    themesRoot      = path.resolve(appRoot, 'content/themes');
 
 function init () {
 
     // Make sure NODE_ENV is always setup as it is used by express
-    var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
-        defaultTheme = 'casper';
+    var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-    cfg.env({ separator: '___' }).add('memory');
+    // Set user environment as the first configuration component 
+    // and memory configuration to be updated during runtime
+    cfg
+        .env({ separator: '___' })
+        .add('memory');
 
+    // Use the current environment corresponding configuration
     if (config[env]) {
-        cfg.add('userconf', { type: 'literal', store: config[env] });
+        cfg.add('userconf', { type:'literal', store:config[env] });
     }
 
-    cfg.defaults({
-        theme: {
-            name: 'casper', 
-            viewsPath: path.join(themesRoot, 'casper'),
-            staticPath: path.join(themesRoot, defaultTheme, 'assets')
-        },
-        url: 'http://127.0.0.1',
-        server: { host: '127.0.0.1', port: 3000 },
-        database: { type: 'memory' }
-    });
+    // Defauls not provided by previous configuration components
+    cfg.defaults( require('./defaults') );
 
-    cfg.set('env', env);
-    cfg.set('is:development', 'development' === env);
-    cfg.set('is:production', 'production' === env);
+    updateFlags();
+    updatePaths();
+
+    // console.info(require('util').inspect(cfg, {depth: 5}))
 
     return when();
+}
+
+function updatePaths () {
+    var themeName = cfg.get('theme:name');
+
+    cfg.set('theme:viewsPath', path.resolve(themesRoot, themeName));
+    cfg.set('theme:staticPath', path.resolve(themesRoot, themeName, 'assets'));
+}
+
+function updateFlags () {
+    var env = process.env.NODE_ENV;
+
+    cfg.set('env', env);
+    cfg.set('development', 'development' === env);
+    cfg.set('production', 'production' === env);
 }
 
 module.exports.init = init;
