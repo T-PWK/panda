967d9c214da9d82afecd1f65ce1dfca9140ff5a8
Tom Pawlak <tompwk@gmail.com>
2014-02-09 22:17:20 +0000

    New properties added to Post schama

--- core/server/services/postprovider/postprovider-mongo/post-schema.js
+++ core/server/services/postprovider/postprovider-mongo/post-schema.js
@@ -1,16 +1,28 @@
 var mongoose = require('mongoose');
 
 var postSchema = mongoose.Schema({
+    // # Content properties
     slug: { type: String, required: true },
     title: { type: String, required: true },
+    content: String,
+    excerpt: String,
+    labels: { type: [String] },
+
+    // # Metadata
+    metaTitle: String,
+    metaDescription: String,
+
+    // # Dates
     created: { type: Date, required: true, default: Date.now },
     updated: { type: Date, required: true, default: Date.now },
     scheduled: { type: Date, default: Date.now },
-    labels: { type: [String] },
+    
+    // # Additional properties
     geo: { type: [Number] },
     hidden: { type: Boolean, default:false },
-    content: String,
-    excerpt: String,
+    page: Boolean,
+
+    // # Conters
     counter: {
         view: { type: Number, default: 0 },
         edit: { type: Number, default: 0 }
