1affcccfe2b21c56025c76eda00ac3cd0daa5f9e
Tom Pawlak <tompwk@gmail.com>
2014-02-13 18:45:29 +0000

    Blog class renamed to more generic Application

--- core/server/models/blog.js
+++
@@ -1,9 +0,0 @@
-var _ = require('underscore');
-
-function Blog () {
-    var args = Array.prototype.slice.call(arguments);
-    args.unshift(this);
-    _.extend.apply(_, args)
-};
-
-module.exports = Blog;
