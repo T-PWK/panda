be72c7d16cac0a8a1821d2709f2d72dd1add9e76
Tom Pawlak <tompwk@gmail.com>
2014-02-13 19:01:14 +0000

    Pagination changes added

--- core/server/helpers/pagination.js
+++ core/server/helpers/pagination.js
@@ -1,14 +1,16 @@
 var cfg = require('nconf');
 
-module.exports = function (posts, page) {
+module.exports = function (req, posts) {
 
     var howMany = cfg.get('app:postsPerPage'),
-        pages = Math.ceil(posts.length / howMany);
+        pages = Math.ceil(posts / howMany),
+        page = +req.params.page || 1;
 
     return {
+        context: req.path,
         pages: pages,
         page: page,
-        older: page < pages ? { page: page + 1 } : undefined,
-        newer: page > 1 ? { page: page - 1 } : undefined
+        older: page < pages ? page + 1 : undefined,
+        newer: page > 1 ? page - 1 : undefined
     }
 }
