dfe049a8f274b5321d057d1e9b0363289e5fa9a5
Tom Pawlak <tompwk@gmail.com>
2014-02-15 02:24:52 +0000

    Application model has been removed; app.locales and res.locales are used instead

--- core/server/models/application.js
+++
@@ -1,9 +0,0 @@
-var _ = require('underscore');
-
-function Application () {
-    var args = Array.prototype.slice.call(arguments);
-    args.unshift(this);
-    _.extend.apply(_, args)
-};
-
-module.exports = Application;
