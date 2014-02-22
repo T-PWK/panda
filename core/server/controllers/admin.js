3caddf2268a46cc87871ebaa553bb07bab402eec
Tom Pawlak <tompwk@gmail.com>
2014-02-22 21:11:41 +0000

    Admin controller, routes and views added

---
+++ core/server/controllers/admin.js
@@ -0,0 +1,7 @@
+var cfg     = require('nconf'),
+    join    = require('path').join
+
+
+module.exports.index = function (req, res) {
+    res.render(join(cfg.get('paths:adminViews'), 'admin'));
+}
---
+++ core/server/routes/admin.js
@@ -0,0 +1,5 @@
+var admin = require('../controllers/admin');
+
+module.exports = function (server) {
+    server.get('/admin', admin.index);
+}
