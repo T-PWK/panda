fcf21450e65b6c0b799c4f2629b4b330b84b34e9
Tom <pawlaktom@gmail.com>
2014-03-07 14:26:21 +0000

    Atom and RSS feed update

--- core/server/controllers/feeds.js
+++ core/server/controllers/feeds.js
@@ -1,20 +1,29 @@
 var when          = require('when'),
     join          = require('path').join,
-    provider      = require('../providers').postProvider;
+    provider      = require('../providers').postProvider,
+    userProvider  = require('../providers').userProvider;
+
+require('pkginfo')(module, 'version');
 
 function view (req, name) {
     return join(req.app.get('admin views'), name);
 }
 
 module.exports.rss = function (req, res) {
-    when(provider.findAll())
-        .then(function (posts) {
-            res.locals({ posts: posts });
-            res.type('rss').render(view(req, 'rss'));
-        })
-        .catch(function (err) {
-            res.send(500);
-        });
+    when.join(
+        provider.findAll(), userProvider.findLeadUser()
+    )
+    .spread(function (posts, user) {
+
+        res.locals.posts = posts;
+        res.locals.author = user;
+        res.locals.version = module.exports.version;
+
+        res.type('rss').render(view(req, 'rss'));
+    })
+    .catch(function (err) {
+        res.send(500);
+    });
 };
 
 module.exports.atom = function (req, res) {
@@ -23,10 +32,9 @@ module.exports.atom = function (req, res) {
 
             res.locals.posts = posts;
             res.locals.updated = maxUpdatedDate(posts);
+            res.locals.version = module.exports.version;
 
-            res
-                .type('atom')
-                .render(view(req, 'atom'));
+            res.type('atom').render(view(req, 'atom'));
         })
         .catch(function (err) {
             res.send(500);
