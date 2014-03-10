0d6a69d9d81e0d564ba32a8bd100540843e36c63
Tom <pawlaktom@gmail.com>
2014-03-10 17:10:04 +0000

    Admin application update
    Initial commit of admin application files

--- core/client/js/application.js
+++ core/client/js/application.js
@@ -3,6 +3,13 @@
 angular.module('panda', ['ngRoute']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider
-      .when('/', { templateUrl:'/admin/partial/main', controller:MainCtrl })
+      .when('/', { templateUrl:'/admin/partial/overview', controller:OverviewCtrl })
+      .when('/posts', { templateUrl:'/admin/partial/posts', controller:PostsCtrl })
+      .when('/posts/all', { templateUrl:'/admin/partial/posts', controller:PostsCtrl })
+      .when('/posts/scheduled', { templateUrl:'/admin/partial/posts', controller:PostsCtrl })
+      .when('/posts/draft', { templateUrl:'/admin/partial/posts', controller:PostsCtrl })
+      .when('/posts/published', { templateUrl:'/admin/partial/posts', controller:PostsCtrl })
+      .when('/pages', { templateUrl:'/admin/partial/pages', controller:PagesCtrl })
+      .when('/settings', { templateUrl:'/admin/partial/settings', controller:SettingsCtrl })
       .otherwise({redirectTo: '/'});
 }]);
--- core/client/js/controllers.js
+++ core/client/js/controllers.js
@@ -1,6 +1,43 @@
 'use strict';
 
-function MainCtrl ($scope)  {
+function RootCtrl ($scope) {
+    $scope.pages = [];
+
+    $scope.setPages = function (props) {
+        var args = Array.prototype.slice.call(arguments);
+        $scope.pages.splice(0, $scope.pages.length);
+
+        console.info(args.length / 2)
+        
+        for (var i = 0; i <= args.length / 2; i+=2) {
+            $scope.pages.push({id:args[i], name:args[i+1]});
+        };
+    }
 }
 
-MainCtrl.$inject = ['$scope'];
+RootCtrl.$inject = ['$scope'];
+
+function OverviewCtrl ($scope)  {
+    $scope.setPages('overview', 'Overview');
+}
+
+OverviewCtrl.$inject = ['$scope'];
+
+function PostsCtrl ($scope) {
+    $scope.setPages('posts', 'Posts', 'posts-all', 'All (20)');
+}
+
+PostsCtrl.$inject = ['$scope'];
+
+
+function PagesCtrl ($scope) {
+    $scope.setPages('pages', 'Pages');
+}
+
+PagesCtrl.$inject = ['$scope'];
+
+function SettingsCtrl ($scope) {
+    $scope.setPages('settings', 'Settings');
+}
+
+SettingsCtrl.$inject = ['$scope'];
