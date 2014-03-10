f682e7648eb1c59094c6e075b5c42914a84f04dc
Tom <pawlaktom@gmail.com>
2014-03-10 14:58:11 +0000

    Routes optimization

--- core/server/routes/frontend.js
+++ core/server/routes/frontend.js
@@ -9,6 +9,12 @@ module.exports = function (server) {
     server.param('page', frontend.pageParam);
 
     server.get('/', frontend.middleware, frontend.index);                           // Main page
+    server.get(cfg.get('app:paginationUrl'), frontend.middleware, frontend.index);  // Pagination
+
+    // year
+    server.get('/:year', frontend.middleware, frontend.year);
+
+    // slug
     server.get(cfg.get('app:postUrl'), frontend.middleware, frontend.post);         // Post page
 
     // Use static page URL only if it is different than postUrl
@@ -16,26 +22,29 @@ module.exports = function (server) {
         server.get(cfg.get('app:pageUrl'), frontend.middleware, frontend.post);     // Post page
     }
 
-    server.get(cfg.get('app:paginationUrl'), frontend.middleware, frontend.index);  // Pagination
-
+    // label
     server.get(cfg.get('app:labelUrl'), 
         frontend.middleware, frontend.searchByLabel);
 
+    // label/pagination
     server.get(cfg.get('app:labelUrl') + cfg.get('app:paginationUrl'), 
         frontend.middleware, frontend.searchByLabel);
 
-    server.get('/:year', frontend.middleware, frontend.year);
-
+    // year/pagination
     server.get('/:year' + cfg.get('app:paginationUrl'), 
         frontend.middleware, frontend.year);
 
+    // /year/month
     server.get('/:year/:month', frontend.middleware, frontend.month);
 
+    // /year/month/pagination
     server.get('/:year/:month' + cfg.get('app:paginationUrl'), 
         frontend.middleware, frontend.month);
 
+    // year/month/day
     server.get('/:year/:month/:day', frontend.middleware, frontend.day);
 
+    // year/month/day/pagination
     server.get('/:year/:month/:day' + cfg.get('app:paginationUrl'), 
         frontend.middleware, frontend.day);
 };
