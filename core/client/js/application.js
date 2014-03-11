17fabfc22069bc581f9657a21b380c406222cf04
Tom <pawlaktom@gmail.com>
2014-03-11 13:00:22 +0000

    Admin application changes

--- core/client/js/application.js
+++ core/client/js/application.js
@@ -1,15 +1,16 @@
 'use strict';
 
-angular.module('panda', ['ngRoute']).
+angular.module('panda', ['ngRoute', 'pandaControllers']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider
-      .when('/', { templateUrl:'/admin/partial/overview', controller:OverviewCtrl })
-      .when('/posts', { templateUrl:'/admin/partial/posts', controller:PostsCtrl })
-      .when('/posts/all', { templateUrl:'/admin/partial/posts', controller:PostsCtrl })
-      .when('/posts/scheduled', { templateUrl:'/admin/partial/posts', controller:PostsCtrl })
-      .when('/posts/draft', { templateUrl:'/admin/partial/posts', controller:PostsCtrl })
-      .when('/posts/published', { templateUrl:'/admin/partial/posts', controller:PostsCtrl })
-      .when('/pages', { templateUrl:'/admin/partial/pages', controller:PagesCtrl })
-      .when('/settings', { templateUrl:'/admin/partial/settings', controller:SettingsCtrl })
+      .when('/', { templateUrl:'/admin/partial/overview', controller:'OverviewCtrl' })
+      .when('/posts', { templateUrl:'/admin/partial/posts', controller:'PostsCtrl' })
+      .when('/posts/all', { templateUrl:'/admin/partial/posts', controller:'PostsCtrl' })
+      .when('/posts/scheduled', { templateUrl:'/admin/partial/posts', controller:'PostsCtrl' })
+      .when('/posts/draft', { templateUrl:'/admin/partial/posts', controller:'PostsCtrl' })
+      .when('/posts/published', { templateUrl:'/admin/partial/posts', controller:'PostsCtrl' })
+      .when('/pages', { templateUrl:'/admin/partial/pages', controller:'PagesCtrl' })
+      .when('/comments', { templateUrl:'/admin/partial/comments', controller:'CommentsCtrl' })
+      .when('/settings', { templateUrl:'/admin/partial/settings', controller:'SettingsCtrl' })
       .otherwise({redirectTo: '/'});
 }]);
--- core/client/js/controllers.js
+++ core/client/js/controllers.js
@@ -1,43 +1,75 @@
 'use strict';
 
-function RootCtrl ($scope) {
-    $scope.pages = [];
+var ctrlsModule = angular.module('pandaControllers', []);
 
-    $scope.setPages = function (props) {
-        var args = Array.prototype.slice.call(arguments);
-        $scope.pages.splice(0, $scope.pages.length);
+ctrlsModule.controller('RootCtrl', ['$scope', 
+    function ($scope) {
+        $scope.pages = [];
 
-        console.info(args.length / 2)
-        
-        for (var i = 0; i <= args.length / 2; i+=2) {
-            $scope.pages.push({id:args[i], name:args[i+1]});
+        $scope.setPages = function (props) {
+            var args = Array.prototype.slice.call(arguments);
+            $scope.pages.splice(0, $scope.pages.length);
+
+            console.info(args.length / 2)
+            
+            for (var i = 0; i <= args.length / 2; i+=2) {
+                $scope.pages.push({id:args[i], name:args[i+1]});
+            };
         };
     }
-}
+]);
 
-RootCtrl.$inject = ['$scope'];
+ctrlsModule.controller('OverviewCtrl', ['$scope', 
+    function ($scope) {
+        $scope.setPages('overview', 'Overview');
+    }
+]);
 
-function OverviewCtrl ($scope)  {
-    $scope.setPages('overview', 'Overview');
-}
+ctrlsModule.controller('PostsCtrl', ['$scope',
+    function function_name ($scope) {
+        $scope.setPages('posts', 'Posts', 'posts-all', 'All (20)');
+        $scope.itemsPerPage = 10;
 
-OverviewCtrl.$inject = ['$scope'];
+        $scope.posts = [
+            {title:'Post title', state:'Draft', author:{name:'User Name', website:'http://google.com'}, comments: 2, publishedAt: new Date()},
+            {title:'Debug express', state:'Draft', author:{name:'User Name', website:'http://google.com'}, comments: 0, publishedAt: new Date()},
+            {title:'Meta Keywords: Should we use them or not?', state:'Published', author:{name:'User Name', website:'http://google.com'}, comments: 0, publishedAt: new Date(2013, 5, 26, 3, 1)},
+            {title:'Promise with node.js Loading file', state:'Published', author:{name:'User Name', website:'http://google.com'}, comments: 1, publishedAt: new Date(2013, 1, 20, 12, 33)}
+        ];
 
-function PostsCtrl ($scope) {
-    $scope.setPages('posts', 'Posts', 'posts-all', 'All (20)');
-}
+        $scope.setItemsPerPage = function (count, e) {
+            e.preventDefault();
+            $scope.itemsPerPage = count;
+        };
+    }
+]);
 
-PostsCtrl.$inject = ['$scope'];
+ctrlsModule.controller('PagesCtrl', ['$scope', '$interval', 
+    function ($scope, $interval) {
+        $scope.setPages('pages', 'Pages');
+        $scope.publish = true;
 
+        $interval(function () {
+            $scope.publish = !$scope.publish;
+        }, 20000);
 
-function PagesCtrl ($scope) {
-    $scope.setPages('pages', 'Pages');
-}
+        $scope.pages = [
+            {title:'Post title', state:'Draft', user:{name:'User Name', website:'http://google.com'}, comments: 0, publishedAt: new Date()},
+            {title:'Debug express', state:'Draft', user:{name:'User Name', website:'http://google.com'}, comments: 0, publishedAt: new Date()},
+            {title:'Meta Keywords: Should we use them or not?', state:'Published', user:{name:'User Name', website:'http://google.com'}, comments: 0, publishedAt: new Date(2013, 5, 26, 3, 1)},
+            {title:'Promise with node.js Loading file', state:'Published', user:{name:'User Name', website:'http://google.com'}, comments: 0, publishedAt: new Date(2013, 0, 20, 12, 33)}
+        ];
+    }
+]);
 
-PagesCtrl.$inject = ['$scope'];
+ctrlsModule.controller('SettingsCtrl', ['$scope', 
+    function ($scope) {
+        $scope.setPages('settings', 'Settings');
+    }
+]);
 
-function SettingsCtrl ($scope) {
-    $scope.setPages('settings', 'Settings');
-}
-
-SettingsCtrl.$inject = ['$scope'];
+ctrlsModule.controller('CommentsCtrl', ['$scope', 
+    function ($scope) {
+        $scope.setPages('comments', 'Comments');
+    }
+]);
