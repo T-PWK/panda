c6078e4728ae86c863448ca72a4463154ecc3054
Tom <pawlaktom@gmail.com>
2014-03-12 17:04:34 +0000

    API Updates

--- core/server/controllers/api.js
+++ core/server/controllers/api.js
@@ -3,16 +3,25 @@ var when             = require('when'),
 
 module.exports.posts = function (req, res) {
     provider
-        .fetchAll({ post: true, limit: +req.query.limit, sortBy: req.query.sortBy })
-        .then(function (posts) {
-            res.json(posts);
-        });
+        .fetchAll({
+            post: true, 
+            limit: +req.query.limit, 
+            sortBy: req.query.sortBy,
+            type: req.query.type
+        })
+        .then(res.json.bind(res));
+}
+
+module.exports.postsCountByPublishedAt = function (req, res) {
+    provider.countByPublishedAt({ post: true }).then(res.json.bind(res));
+}
+
+module.exports.pagesCountByPublishedAt = function (req, res) {
+    provider.countByPublishedAt({ post: false }).then(res.json.bind(res));
 }
 
 module.exports.pages = function (req, res) {
     provider
         .fetchAll({ post: false, limit: +req.query.limit, sortBy: req.query.sortBy })
-        .then(function (pages) {
-            res.json(pages);
-        });
+        .then(res.json.bind(res));
 }
--- core/server/routes/api.js
+++ core/server/routes/api.js
@@ -1,6 +1,11 @@
 var api = require('../controllers/api');
 
 module.exports = function (server) {
+    // Posts
     server.get('/api/v1/posts', api.posts);
+    server.get('/api/v1/posts/count/published', api.postsCountByPublishedAt);
+
+    // Pages
     server.get('/api/v1/pages', api.pages);
+    server.get('/api/v1/pages/count/published', api.pagesCountByPublishedAt);
 };
