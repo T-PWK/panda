3dff2ba5a47daa5b8f05c001daedc01be81013cf
Tom Pawlak <tompwk@gmail.com>
2014-03-12 08:18:43 +0000

    API pages section added

--- core/server/controllers/api.js
+++ core/server/controllers/api.js
@@ -3,8 +3,16 @@ var when             = require('when'),
 
 module.exports.posts = function (req, res) {
     provider
-        .findAll({ limit: +req.query.limit, sortBy: req.query.sortBy })
+        .fetchAll({ post: true, limit: +req.query.limit, sortBy: req.query.sortBy })
         .then(function (posts) {
             res.json(posts);
+        });
+}
+
+module.exports.pages = function (req, res) {
+    provider
+        .fetchAll({ post: false, limit: +req.query.limit, sortBy: req.query.sortBy })
+        .then(function (pages) {
+            res.json(pages);
         });
 }
--- core/server/routes/api.js
+++ core/server/routes/api.js
@@ -2,4 +2,5 @@ var api = require('../controllers/api');
 
 module.exports = function (server) {
     server.get('/api/v1/posts', api.posts);
+    server.get('/api/v1/pages', api.pages);
 };
