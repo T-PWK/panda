efa4b77060e8b8248540062c6380e563176eb37d
Tom Pawlak <tompwk@gmail.com>
2014-03-14 17:37:51 +0000

    Login handler added

--- core/server/controllers/admin.js
+++ core/server/controllers/admin.js
@@ -6,6 +6,10 @@ module.exports.index = function (req, res) {
     res.render(join(cfg.get('paths:adminViews'), 'admin'));
 };
 
+module.exports.login = function (req, res) {
+    res.render(join(cfg.get('paths:adminViews'), 'admin/login'));
+};
+
 module.exports.partial = function (req, res) {
     res.render(join(cfg.get('paths:adminViews'), 'admin/partial/'+req.params.name ));
 };
--- core/server/routes/admin.js
+++ core/server/routes/admin.js
@@ -2,5 +2,6 @@ var admin = require('../controllers/admin');
 
 module.exports = function (server) {
     server.get('/admin', admin.index);
+    server.get('/admin/login', admin.login);
     server.get('/admin/partial/:name', admin.partial);
 };
