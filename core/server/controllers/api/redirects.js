f1fcfc4f9b3e67fcee1533f0d37edd1be9ea6ade
Tom <pawlaktom@gmail.com>
2014-03-20 15:13:34 +0000

    Redirects API Updates
    Creation and update of redirects added

--- core/server/controllers/api/redirects.js
+++ core/server/controllers/api/redirects.js
@@ -18,9 +18,28 @@ module.exports.show = function (req, res) {
         );
 };
 
+module.exports.update = function (req, res) {
+    provider
+        .updateRedirect(req.params.redirect, req.body)
+        .done(
+            res.send.bind(res, 200),
+            res.send.bind(res, 500)
+        );
+};
+
 module.exports.destroy = function (req, res) {
     provider
         .deleteRedirect(req.params.redirect)
+        .done(
+            res.json.bind(res),
+            res.send.bind(res, 500)
+        );
+};
+
+module.exports.create = function (req, res) {
+    console.info('creating redirect ...', req.body)
+    provider
+        .createRedirect(req.body)
         .done(
             res.json.bind(res),
             res.send.bind(res, 500)
