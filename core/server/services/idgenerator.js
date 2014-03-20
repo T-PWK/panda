228ea6875f1c575e74494258ac635835695c159e
Tom <pawlaktom@gmail.com>
2014-03-20 15:11:54 +0000

    ID Generator service added

---
+++ core/server/services/idgenerator.js
@@ -0,0 +1,7 @@
+var flakeIdGen  = require('flake-idgen'),
+    format      = require('biguint-format'),
+    generator   = new flakeIdGen();
+
+module.exports = function () {
+    return format(generator.next(), 'dec');
+};
