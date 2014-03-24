c2909a6d4728c114915da1dcc83acc6d36f67690
Tom <pawlaktom@gmail.com>
2014-03-24 15:23:40 +0000

    AdminThemes file uses strict mode

--- core/server/controllers/api/adminthemes.js
+++ core/server/controllers/api/adminthemes.js
@@ -1,53 +1,61 @@
-var cfg         = require('nconf'),
-    fs          = require('fs'),
-    _           = require('underscore'),
-    _s          = require('underscore.string'),
-    when        = require('when'),
-    node        = require('when/node'),
-    readdir     = node.lift(fs.readdir),
-    join        = require('path').join,
-    themesDir   = join(cfg.get('paths:clientStatic'), 'css/theme'); /* jshint -W030 */
+(function () {
+    'use strict';
 
-module.exports.index = function (req, res) {
-    readdir(themesDir)
-        .then(namesToThemes)
-        .tap(selectActive)
-        .done(
-            res.json.bind(res),
-            res.send.bind(res, 500)
-        );
+    var cfg         = require('nconf'),
+        fs          = require('fs'),
+        _           = require('underscore'),
+        _s          = require('underscore.string'),
+        when        = require('when'),
+        node        = require('when/node'),
+        readdir     = node.lift(fs.readdir),
+        join        = require('path').join,
+        themesDir   = join(cfg.get('paths:clientStatic'), 'css/theme'); /* jshint -W030 */
 
-    function selectActive (themes) {
-        var active = _.findWhere(themes, { id:cfg.get('admin:theme') });
-        if (active) active.active = true;
-    }
+    module.exports.index = function (req, res) {
+        readdir(themesDir)
+            .then(namesToThemes)
+            .tap(selectActive)
+            .done(
+                res.json.bind(res),
+                res.send.bind(res, 500)
+            );
 
-    function nameToTheme (name) {
-        return {
-            id: name,
-            name: _s.titleize(_s.humanize(name))
-        };
-    }
+        function selectActive (themes) {
+            var active = _.findWhere(themes, { id:cfg.get('admin:theme') });
+            if (active) {
+                active.active = true;
+            }
+        }
 
-    function namesToThemes (fileNames) {
-        return fileNames.map(nameToTheme);
-    }
-};
+        function nameToTheme (name) {
+            return {
+                id: name,
+                name: _s.titleize(_s.humanize(name))
+            };
+        }
 
-module.exports.update = function (req, res) {
-    readdir(themesDir)
-        .then(checkThemes)
-        .then(setAdminTheme)
-        .done(
-            res.json.bind(res),
-            res.send.bind(res, 400)
-        );
+        function namesToThemes (fileNames) {
+            return fileNames.map(nameToTheme);
+        }
+    };
 
-    function checkThemes (themes) {
-        return (themes.indexOf(req.params.admin) < 0) ? when.reject() : req.params.admin;
-    }
+    module.exports.update = function (req, res) {
+        readdir(themesDir)
+            .then(checkThemes)
+            .then(setAdminTheme)
+            .done(
+                res.json.bind(res),
+                res.send.bind(res, 400)
+            );
 
-    function setAdminTheme (theme) {
-        cfg.set('admin:theme', req.params.admin);
-    }
-};
+        function checkThemes (themes) {
+            return (themes.indexOf(req.params.admin) < 0) ? when.reject() : req.params.admin;
+        }
+
+        function setAdminTheme (theme) {
+            cfg.set('admin:theme', req.params.admin);
+        }
+    };
+
+}());
+
