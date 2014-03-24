11d1df8ce5aa900b560cdb5fff624a4e2dfd94bd
Tom Pawlak <tompwk@gmail.com>
2014-03-24 21:49:49 +0000

    Strict mode used

--- core/server/services/postprovider/postprovider-memory.js
+++ core/server/services/postprovider/postprovider-memory.js
@@ -1,19 +1,21 @@
 // PostProvider using memory storage
+(function () {
+    'use strict';
 
-var _           = require('underscore'),
-    when        = require('when'),
-    nodefn      = require('when/node/function'),
-    cfg         = require('nconf'),
-    fs          = require('fs'),
-    path        = require('path'),
-    moment      = require('moment'),
-    idGen       = require('../idgenerator');
-    isAbsolute  = require('../../utils').isAbsolute,
-    dateProps   = ['publishedAt', 'createdAt', 'updatedAt'];
+    var _           = require('underscore'),
+        when        = require('when'),
+        nodefn      = require('when/node/function'),
+        cfg         = require('nconf'),
+        fs          = require('fs'),
+        path        = require('path'),
+        moment      = require('moment'),
+        idGen       = require('../idgenerator'),
+        isAbsolute  = require('../../utils').isAbsolute,
+        dateProps   = ['publishedAt', 'createdAt', 'updatedAt'];
 
-var PostProvider = module.exports = function () {
-    this.dummyData = [];
-};
+    var PostProvider = module.exports = function () {
+        this.dummyData = [];
+    };
 
 // PostProvider
 // init
@@ -30,253 +32,258 @@ var PostProvider = module.exports = function () {
 // labelsInfo
 // postCountInfo
 
-PostProvider.prototype.init = function () {
-    return this.loadFiles();
-};
-
-PostProvider.prototype.loadFiles = function () {
-    var that        = this,
-        postsPath   = cfg.get('database:postsFile'),
-        usersPath   = cfg.get('database:usersFile'),
-        postsFile   = isAbsolute(postsPath) ? postsPath : path.join(cfg.get('paths:data'), postsPath),
-        usersFile   = isAbsolute(usersPath) ? usersPath : path.join(cfg.get('paths:data'), usersPath),
-        loadPosts   = nodefn.call(fs.readFile, postsFile),
-        loadUsers   = nodefn.call(fs.readFile, usersFile);
-
-    return when
-        .join(loadPosts, loadUsers)                 // load data files
-        .then(function (values) {                   // parse each file to JSON
-            return when.map(values, JSON.parse);
-        })
-        .spread(function (posts, users) {           // set internal properties
-            that.dummyData = posts;
-            that.users = users;
-        })
-        .then(convertDateProperties.bind(that))     // convert date properties
-        .then(updateAuthorInfo.bind(that));         // update users (authors)
+    PostProvider.prototype.init = function () {
+        return this.loadFiles();
     };
 
-PostProvider.prototype.create = function(post) {
-    post._id = idGen();
+    PostProvider.prototype.loadFiles = function () {
+        var that        = this,
+            postsPath   = cfg.get('database:postsFile'),
+            usersPath   = cfg.get('database:usersFile'),
+            postsFile   = isAbsolute(postsPath) ? postsPath : path.join(cfg.get('paths:data'), postsPath),
+            usersFile   = isAbsolute(usersPath) ? usersPath : path.join(cfg.get('paths:data'), usersPath),
+            loadPosts   = nodefn.call(fs.readFile, postsFile),
+            loadUsers   = nodefn.call(fs.readFile, usersFile);
 
-    this.dummyData.push(post);
+        return when
+            .join(loadPosts, loadUsers)                 // load data files
+            .then(function (values) {                   // parse each file to JSON
+                return when.map(values, JSON.parse);
+            })
+            .spread(function (posts, users) {           // set internal properties
+                that.dummyData = posts;
+                that.users = users;
 
-    console.info('created post ... ', post);
-    console.info('all posts', this.dummyData.length);
+                convertDateProperties(posts);
+                updateAuthorInfo(posts, users);
+            });
+    };
 
-    return when.resolve(post._id);
-};
+    PostProvider.prototype.create = function(post) {
+        post._id = idGen();
 
-/*
- * Find all posts
- */
-PostProvider.prototype.findAll = function (opts) {
-    return sortAndSlice(select(this.dummyData, opts), opts);
-};
+        this.dummyData.push(post);
 
-/*
- * Finds a single post by slug
- */
-PostProvider.prototype.findBySlug = function (slug, opts) {
-    var chain = _.chain(this.dummyData).filter(function (post) {
-        return post.slug === slug;
-    });
+        console.info('created post ... ', post);
+        console.info('all posts', this.dummyData.length);
 
-    return when.resolve(updateSelection(chain, opts).first().value());
-};
+        return when.resolve(post._id);
+    };
 
-/*
- * Finds a single post by id
- */
-PostProvider.prototype.findById = function (id, opts) {
-    var chain = _.chain(this.dummyData).filter(function (post) {
-        return post._id === id;
-    });
+    /*
+     * Find all posts
+     */
+    PostProvider.prototype.findAll = function (opts) {
+        return sortAndSlice(select(this.dummyData, opts), opts);
+    };
 
-    return when.resolve(updateSelection(chain, opts).first().value());
-};
+    /*
+     * Finds a single post by slug
+     */
+    PostProvider.prototype.findBySlug = function (slug, opts) {
+        var chain = _.chain(this.dummyData).filter(function (post) {
+            return post.slug === slug;
+        });
 
-/*
- * Finds posts at a given date range (from start to end of a year, or a month, or a day)
- */
-PostProvider.prototype.findByDate = function (opts) {
-    return sortAndSlice(select(this.dummyData, opts), opts);
-};
+        return when.resolve(updateSelection(chain, opts).first().value());
+    };
 
-PostProvider.prototype.findByLabel = function (opts) {
-    if (!opts.label) return when.resolve([]);
+    /*
+     * Finds a single post by id
+     */
+    PostProvider.prototype.findById = function (id, opts) {
+        var chain = _.chain(this.dummyData).filter(function (post) {
+            return post._id === id;
+        });
 
-    return sortAndSlice(select(this.dummyData, opts), opts);
-};
+        return when.resolve(updateSelection(chain, opts).first().value());
+    };
 
-PostProvider.prototype.count = function (opts) {
-    return when.resolve(select(this.dummyData, opts).length);
-};
+    /*
+     * Finds posts at a given date range (from start to end of a year, or a month, or a day)
+     */
+    PostProvider.prototype.findByDate = function (opts) {
+        return sortAndSlice(select(this.dummyData, opts), opts);
+    };
 
-function updateSelection (chain, opts) {
-    if (!opts) return chain;
+    PostProvider.prototype.findByLabel = function (opts) {
+        if (!opts.label) return when.resolve([]);
 
-    var now = Date.now();
+        return sortAndSlice(select(this.dummyData, opts), opts);
+    };
 
-    if (opts.live || 'live' === opts.type) {
-        chain = chain.filter(function (post) {
-            return post.publishedAt <= now;
+    PostProvider.prototype.count = function (opts) {
+        return when.resolve(select(this.dummyData, opts).length);
+    };
+
+    function updateSelection (chain, opts) {
+        if (!opts) return chain;
+
+        var now = Date.now();
+
+        if (opts.live || 'live' === opts.type) {
+            chain = chain.filter(function (post) {
+                return post.publishedAt <= now;
+            });
+        }
+
+        if('undefined' !== typeof opts.page) {
+            chain = chain.filter(function (post) {
+                return post.page === opts.page;
+            });
+        }
+
+        return chain;
+    }
+
+    function select(posts, opts) {
+        opts = opts || {};
+
+        var month = (opts.month || 1) - 1, // JavaScript Date uses 0-based month index
+            now = new Date(),
+            chain = _.chain(posts);
+
+        chain = updateSelection(chain, opts);
+
+        if (opts.label) {
+            chain = chain.filter(function (post) {
+                return post.labels && post.labels.indexOf(opts.label) >= 0;
+            });
+        }
+
+        if (opts.year) {
+            chain = chain.filter(function (post) {
+                return post.publishedAt && post.publishedAt.getFullYear() === opts.year;
+            });
+        }
+
+        if (opts.month) {
+            chain = chain.filter(function (post) {
+                return post.publishedAt && post.publishedAt.getMonth() === month;
+            });
+        }
+
+        if (opts.day) {
+            chain = chain.filter(function (post) {
+                return post.publishedAt && post.publishedAt.getDate() === opts.day;
+            });
+        }
+
+        if ('draft' === opts.type) {
+            chain = chain.filter(function (post) {
+                return !post.publishedAt;
+            });
+        }
+
+        if ('scheduled' === opts.type) {
+            chain = chain.filter(function (post) {
+                return post.publishedAt > now;
+            });
+        }
+
+        return chain.value();
+    }
+
+    PostProvider.prototype.postCountInfo = function (opts) {
+        var items, now = Date.now(),
+            chain = updateSelection(_.chain(this.dummyData), opts),
+            count = chain.reduce(function (count, post) {
+                if (!post.publishedAt) count.draft++;
+                else if (post.publishedAt > now) count.scheduled++;
+                else count.live++;
+
+                return count;
+            }, { all: 0, live: 0, draft: 0, scheduled: 0 }).value();
+
+        count.all = count.live + count.scheduled + count.draft;
+
+        return when.resolve(count);
+    };
+
+    PostProvider.prototype.archiveInfo = function (opts) {
+        var posts = select(this.dummyData, opts);
+
+        var info = _.chain(posts)
+            .map(function (post) {
+                return moment(post.publishedAt).startOf('month').valueOf();
+            })
+            .groupBy()
+            .map(function (values, date) {
+                return {
+                    dateMillisec: +date,
+                    date: moment(+date),
+                    count: values.length
+                };
+            })
+            .sortBy(function (item) {
+                return -item.dateMillisec;
+            })
+            .value();
+
+        return when.resolve(info);
+    };
+
+    PostProvider.prototype.labelsInfo = function (opts) {
+        var labels = _.chain(select(this.dummyData, opts))
+            .map(function (post) {
+                return post.labels;
+            })
+            .flatten()
+            .countBy()
+            .map(function (count, label) {
+                return { label: label, count: count };
+            })
+            .sortBy(function (item) {
+                return -item.count;
+            })
+            .value();
+
+        return when.resolve(labels);
+    };
+
+    function sortAndSlice(posts, opts) {
+        opts = opts || {};
+
+        var start = opts.skip || 0,
+            end = (opts.limit) ? start + opts.limit : undefined;
+
+        return when.resolve(sort(posts, opts).slice(start, end));
+    }
+
+    function sort(posts, opts) {
+        var reverse = false,
+            sortBy = opts.sortBy || '-publishedAt';
+
+        posts = posts.slice(0);
+
+        if (sortBy[0] === '-') {
+            reverse = true;
+            sortBy = sortBy.slice(1);
+        }
+
+        posts = _.sortBy(posts, sortBy);
+
+        if (reverse) {
+            posts.reverse();
+        }
+
+        return posts;
+    }
+
+    function convertDateProperties (data) {
+        data.forEach(function (post) {
+            dateProps.forEach(function (prop) {
+                if (this[prop]) this[prop] = new Date(this[prop]);
+            }, post);
         });
     }
 
-    if('undefined' !== typeof opts.page) {
-        chain = chain.filter(function (post) {
-            return post.page === opts.page;
+    function updateAuthorInfo (data, users) {
+        data.forEach(function (post) {
+            post.author = users[post._authorId];
         });
     }
 
-    return chain;
-}
+}());
 
-function select(posts, opts) {
-    opts = opts || {};
 
-    var month = (opts.month || 1) - 1, // JavaScript Date uses 0-based month index
-        now = new Date(),
-        chain = _.chain(posts);
-
-    chain = updateSelection(chain, opts);
-
-    if (opts.label) {
-        chain = chain.filter(function (post) {
-            return post.labels && post.labels.indexOf(opts.label) >= 0;
-        });
-    }
-
-    if (opts.year) {
-        chain = chain.filter(function (post) {
-            return post.publishedAt && post.publishedAt.getFullYear() === opts.year;
-        });
-    }
-
-    if (opts.month) {
-        chain = chain.filter(function (post) {
-            return post.publishedAt && post.publishedAt.getMonth() === month;
-        });
-    }
-
-    if (opts.day) {
-        chain = chain.filter(function (post) {
-            return post.publishedAt && post.publishedAt.getDate() === opts.day;
-        });
-    }
-
-    if ('draft' === opts.type) {
-        chain = chain.filter(function (post) {
-            return !post.publishedAt;
-        });
-    }
-
-    if ('scheduled' === opts.type) {
-        chain = chain.filter(function (post) {
-            return post.publishedAt > now;
-        });
-    }
-
-    return chain.value();
-}
-
-PostProvider.prototype.postCountInfo = function (opts) {
-    var items, now = Date.now(),
-        chain = updateSelection(_.chain(this.dummyData), opts),
-        count = chain.reduce(function (count, post) {
-            if (!post.publishedAt) count.draft++;
-            else if (post.publishedAt > now) count.scheduled++;
-            else count.live++;
-
-            return count;
-        }, { all: 0, live: 0, draft: 0, scheduled: 0 }).value();
-
-    count.all = count.live + count.scheduled + count.draft;
-
-    return when.resolve(count);
-};
-
-PostProvider.prototype.archiveInfo = function (opts) {
-    var posts = select(this.dummyData, opts);
-
-    var info = _.chain(posts)
-        .map(function (post) {
-            return moment(post.publishedAt).startOf('month').valueOf();
-        })
-        .groupBy()
-        .map(function (values, date) {
-            return {
-                dateMillisec: +date,
-                date: moment(+date),
-                count: values.length
-            };
-        })
-        .sortBy(function (item) {
-            return -item.dateMillisec;
-        })
-        .value();
-
-    return when.resolve(info);
-};
-
-PostProvider.prototype.labelsInfo = function (opts) {
-    var labels = _.chain(select(this.dummyData, opts))
-        .map(function (post) {
-            return post.labels;
-        })
-        .flatten()
-        .countBy()
-        .map(function (count, label) {
-            return { label: label, count: count };
-        })
-        .sortBy(function (item) {
-            return -item.count;
-        })
-        .value();
-
-    return when.resolve(labels);
-};
-
-function sortAndSlice(posts, opts) {
-    opts = opts || {};
-
-    var start = opts.skip || 0, 
-    end = (opts.limit) ? start + opts.limit : undefined;
-
-    return when.resolve(sort(posts, opts).slice(start, end));
-}
-
-function sort(posts, opts) {
-    var reverse = false,
-        sortBy = opts.sortBy || '-publishedAt';
-
-    posts = posts.slice(0);
-
-    if (sortBy[0] === '-') {
-        reverse = true;
-        sortBy = sortBy.slice(1);
-    }
-
-    posts = _.sortBy(posts, sortBy);
-
-    if (reverse) {
-        posts.reverse();
-    }
-
-    return posts;
-}
-
-function convertDateProperties () {
-    this.dummyData.forEach(function (post) {
-        dateProps.forEach(function (prop) {
-            if (this[prop]) this[prop] = new Date(this[prop]);
-        }, post);
-    });
-}
-
-function updateAuthorInfo () {
-    this.dummyData.forEach(function (post) {
-        post.author = this.users[post._authorId];
-    }, this);
-}
