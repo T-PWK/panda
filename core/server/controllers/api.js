32b8e930bc0660f004a7ea06d762fa9075ce12ee
Tom Pawlak <tompwk@gmail.com>
2014-03-31 22:10:15 +0100

    Strict mode

--- core/server/controllers/api.js
+++ core/server/controllers/api.js
@@ -1,11 +1,13 @@
-var when        = require('when'),
-    provider    = require('../providers').postProvider;
+(function(){
+    "use strict";
 
-module.exports.posts        = require('./api/posts');
-module.exports.themes       = require('./api/themes');
-module.exports.adminthemes  = require('./api/adminthemes');
-module.exports.labels       = require('./api/labels');
-module.exports.postsinfo    = require('./api/postsinfo');
-module.exports.redirects    = require('./api/redirects');
-module.exports.settings     = require('./api/settings');
-module.exports.users        = require('./api/users');
+    module.exports.posts = require('./api/posts');
+    module.exports.themes = require('./api/themes');
+    module.exports.adminthemes = require('./api/adminthemes');
+    module.exports.labels = require('./api/labels');
+    module.exports.postsinfo = require('./api/postsinfo');
+    module.exports.redirects = require('./api/redirects');
+    module.exports.settings = require('./api/settings');
+    module.exports.users = require('./api/users');
+
+}());
