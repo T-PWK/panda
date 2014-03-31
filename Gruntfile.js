cfa33b932827f3e8afd5877af4ef52d9e3df1bef
Tom <pawlaktom@gmail.com>
2014-03-31 15:09:35 +0100

    Grunt concat task

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,29 +21,49 @@ function gruntConfig (grunt) {
                     }
                 },
                 src: [
-                    'core/client/js/app/**/*.js',
+                    'core/client/js/app/**/*.js'
                 ]
+            }
+        },
+        uglify: {
+            dynamic_mappings: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'core/client/js/app/',
+                        src: ['**/*.js'],
+                        dest: 'core/client/js/app/',
+                        ext: '.min.js',
+                        extDot: 'first'
+                    }
+                ]
+            }
+        },
+        concat:{
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            dist: {
+                src: ['core/client/js/app/application.min.js', 'core/client/js/app/controllers.min.js'],
+                dest: 'core/client/js/app/panda.min.js'
             }
         }
     };
 
     grunt.initConfig(config);
 
-    grunt.registerTask('release', 
-        'Release task - creates a final built zip\n',
-        [] 
-    );
-
+    grunt.registerTask('release', 'Release task - creates a final built zip\n', []);
     grunt.registerTask('help',
         'Outputs help information if you type `grunt help` instead of `grunt --help`',
         function () {
             console.log('Type `grunt --help` to get the details of available grunt tasks');
         }
     );
-
     grunt.registerTask('validate', 'Run code validation', ['jshint']);
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 }
 
 module.exports = gruntConfig;
