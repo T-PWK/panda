21648d9f67680042bd3d5e394160144f61858bc1
Tom Pawlak <tompwk@gmail.com>
2014-03-31 22:06:49 +0100

    API settings improvements

--- core/server/controllers/api/settings.js
+++ core/server/controllers/api/settings.js
@@ -1,16 +1,17 @@
 (function () {
     'use strict';
 
-    var cfg         = require('nconf'),
-        when        = require('when'),
-        _           = require('underscore'),
-        parse       = require('url').parse,
-        provider    = require('../../providers').configProvider,
-        mapping     = {
+    var cfg             = require('nconf'),
+        when            = require('when'),
+        _               = require('underscore'),
+        parse           = require('url').parse,
+        provider        = require('../../providers').configProvider,
+        mapping         = {
             title:'app:title',
             description:'app:description',
             postsPerPage:'app:postsPerPage'
-        };
+        },
+        allowedSettings = ['app:postUrl', 'app:pageUrl'];
 
     module.exports.index = function (req, res) {
         var settings = {
@@ -23,6 +24,22 @@
         settings.host = url.hostname;
 
         res.json(settings);
+    };
+
+    module.exports.show = function (req, res) {
+        var response = {
+            key: req.params.setting,
+            value: req.setting
+        };
+        return (req.setting) ? res.json(response) : res.send(404);
+    };
+
+    module.exports.load = function (id, fn) {
+        if (_.contains(allowedSettings, id)) {
+            fn(null, cfg.get(id));
+        } else {
+            fn();
+        }
     };
 
     module.exports.create = function (req, res) {
