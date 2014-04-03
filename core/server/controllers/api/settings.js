bbfbf5dfb643b379fe160f58da4698be4ea6b96d
Tom <pawlaktom@gmail.com>
2014-04-03 12:58:47 +0100

    Configuration changes update
    Pre-processing added

--- core/server/controllers/api/settings.js
+++ core/server/controllers/api/settings.js
@@ -1,41 +1,49 @@
 (function () {
     'use strict';
 
-    var cfg             = require('nconf'),
-        when            = require('when'),
-        _               = require('underscore'),
-        parse           = require('url').parse,
-        provider        = require('../../providers').configProvider,
-        mapping         = {
-            title:'app:title',
-            description:'app:description',
-            postsPerPage:'app:postsPerPage'
-        },
-        allowedSettings = ['app:postUrl', 'app:pageUrl'];
+    var cfg                 = require('nconf'),
+        when                = require('when'),
+        _                   = require('underscore'),
+        url                 = require('url'),
+        provider            = require('../../providers').configProvider,
+        debug               = require('debug')('panda:api:settings'),
+        processors          = {},
+        propertyMap         = {},
+        allowedReadSettings = ['app:postUrl', 'app:pageUrl'];
+
+    propertyMap = {
+        title:'app:title',
+        description:'app:description',
+        postsPerPage:'app:postsPerPage',
+        url: 'url'
+    };
+
+    processors.host = function (properties, name, value) {
+        properties.url = url.format({ host:value, protocol:'http' });
+    };
 
     module.exports.index = function (req, res) {
-        var settings = {
-                title:          cfg.get(mapping.title),
-                description:    cfg.get(mapping.description),
-                postsPerPage:   cfg.get(mapping.postsPerPage)
-            },
-            url = parse(cfg.get('url'));
+        debug('reading basic settings');
 
-        settings.host = url.hostname;
+        var urlCfg      = url.parse(cfg.get('url')),
+            settings    = {
+                host:           urlCfg.host,
+                title:          cfg.get(propertyMap.title),
+                description:    cfg.get(propertyMap.description),
+                postsPerPage:   cfg.get(propertyMap.postsPerPage)
+            };
 
         res.json(settings);
     };
 
     module.exports.show = function (req, res) {
-        var response = {
-            key: req.params.setting,
-            value: req.setting
-        };
-        return (req.setting) ? res.json(response) : res.send(404);
+        debug('reading configuration value %j', req.param.setting);
+
+        return (req.setting) ? res.json({ key: req.params.setting, value: req.setting }) : res.send(404);
     };
 
     module.exports.load = function (id, fn) {
-        if (_.contains(allowedSettings, id)) {
+        if (_.contains(allowedReadSettings, id)) {
             fn(null, cfg.get(id));
         } else {
             fn();
@@ -45,23 +53,33 @@
     module.exports.create = function (req, res) {
         var promises = [];
 
+        // Properties pre-processing
+        if (_.size(processors)) {
+            _.each(req.body, function (value, property) {
+                if (property in processors) {
+                    processors[property](req.body, property, value);
+                }
+            });
+        }
+
         _.each(req.body, function (value, property) {
 
-            console.info('checking  ', property)
+            if (property in propertyMap && cfg.get(propertyMap[property]) !== value) {
 
-            if (property in mapping && cfg.get(mapping[property]) !== value) {
-                console.info('updating ', mapping[property])
+                debug('updating setting value %j : %j', propertyMap[property], value);
+
                 promises.push(
-                    provider.saveConfig(mapping[property], value)
-                        .then(cfg.set.bind(cfg, mapping[property], value))
+                    provider.saveConfig(propertyMap[property], value)
+                        .then(cfg.set.bind(cfg, propertyMap[property], value))
                 );
             }
         });
 
-        when.all(promises).done(
-            module.exports.index.bind(null, req, res),
-            res.send.bind(res, 500)
-        );
+        when.all(promises)
+            .done(
+                module.exports.index.bind(null, req, res),
+                res.send.bind(res, 500)
+            );
     };
 
 }());
