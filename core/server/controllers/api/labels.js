f836e18d471e710303c6f261c1b0a0ba7ada769d
Tom <pawlaktom@gmail.com>
2014-04-09 14:47:19 +0100

    Strict mode added

--- core/server/controllers/api/labels.js
+++ core/server/controllers/api/labels.js
@@ -1,5 +1,10 @@
-var provider = require('../../providers').postProvider;
+(function () {
+    "use strict";
 
-module.exports.index = function (req, res) {
-    provider.labelsInfo().then(res.json.bind(res));
-};
+    var provider = require('../../providers').postProvider;
+
+    module.exports.index = function (req, res) {
+        provider.labelsInfo().then(res.json.bind(res));
+    };
+
+}());
