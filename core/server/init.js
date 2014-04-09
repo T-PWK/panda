53b3b8d9f7601f81ba8cb55ca90e623269390e0f
Tom <pawlaktom@gmail.com>
2014-04-09 14:47:43 +0100

    String endsWith method added

--- core/server/init.js
+++ core/server/init.js
@@ -6,13 +6,18 @@
 
     module.exports = function () {
         _.mixin({ 'startsWith': startsWith }, { chain: false });
+        _.mixin({ 'endsWith': endsWith }, { chain: false });
 
         return when.resolve();
     };
 
-    function startsWith(string, search, position) {
+    function startsWith(string, prefix, position) {
         position = position || 0;
-        return string.indexOf(search, position) === position;
+        return string.indexOf(prefix, position) === position;
+    }
+
+    function endsWith(string, suffix) {
+        return string.indexOf(suffix, string.length - suffix.length) !== -1;
     }
 
 }());
