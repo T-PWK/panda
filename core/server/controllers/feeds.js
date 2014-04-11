01a9446b2fe6bbacdd9cd30055cd8640b00617cc
Tom Pawlak <tompwk@gmail.com>
2014-04-11 21:19:00 +0100

    Strict mode

--- core/server/controllers/feeds.js
+++ core/server/controllers/feeds.js
@@ -1,69 +1,74 @@
-var when                = require('when'),
-    cfg                 = require('nconf'),
-    join                = require('path').join,
-    provider            = require('../providers').postProvider,
-    userProvider        = require('../providers').userProvider,
-    atomPostPerPage     = cfg.get('app:feeds:atom:postsPerPage'),
-    rssPostPerPage      = cfg.get('app:feeds:rss:postsPerPage');
+(function () {
+    'use strict';
 
-function view (req, name) {
-    return join(req.app.get('admin views'), name);
-}
+    var when            = require('when'),
+        cfg             = require('nconf'),
+        join            = require('path').join,
+        provider        = require('../providers').postProvider,
+        userProvider    = require('../providers').userProvider,
+        atomPostPerPage = cfg.get('app:feeds:atom:postsPerPage'),
+        rssPostPerPage  = cfg.get('app:feeds:rss:postsPerPage');
 
-module.exports.middleware = function (req, res, next) {
-    when.join(
-        provider.getLabelsInfo(),
-        userProvider.findLeadUser(),
-        provider.count()
-    )
-    .spread(function (labels, leadAuthor, totalPosts) {
-        res.locals.labels       = labels;
-        res.locals.leadAuthor   = leadAuthor;
-        res.locals.totalPosts   = totalPosts;
-    })
-    .then(next);
-};
+    function view(req, name) {
+        return join(req.app.get('admin views'), name);
+    }
 
-module.exports.rss = function (req, res) {
-    when.join(
-        provider.findAll({ limit: rssPostPerPage })
-    )
-    .spread(function (posts) {
+    module.exports.middleware = function (req, res, next) {
+        when.join(
+            provider.getLabelsInfo(),
+            userProvider.findLeadUser(),
+            provider.count()
+        )
+        .spread(function (labels, leadAuthor, totalPosts) {
+            res.locals.labels       = labels;
+            res.locals.leadAuthor   = leadAuthor;
+            res.locals.totalPosts   = totalPosts;
+        })
+        .then(next);
+    };
 
-        res.locals.posts        = posts;
-        res.locals.updated      = maxUpdatedDate(posts);
-        res.locals.startIndex   = 0;
-        res.locals.postsPerPage = rssPostPerPage;
+    module.exports.rss = function (req, res) {
+        when.join(
+            provider.findAll({ limit: rssPostPerPage })
+        )
+        .spread(function (posts) {
 
-        res.type('rss').render(view(req, 'rss'));
-    })
-    .catch(function (err) {
-        res.send(500);
-    });
-};
+            res.locals.posts        = posts;
+            res.locals.updated      = maxUpdatedDate(posts);
+            res.locals.startIndex   = 0;
+            res.locals.postsPerPage = rssPostPerPage;
 
-module.exports.atom = function (req, res) {
-    when.join(
-        provider.findAll({ limit: atomPostPerPage })
-    )
-    .spread(function (posts) {
+            res.type('rss').render(view(req, 'rss'));
+        })
+        .catch(function (err) {
+            res.send(500);
+        });
+    };
 
-        res.locals.posts        = posts;
-        res.locals.updated      = maxUpdatedDate(posts);
-        res.locals.startIndex   = 0;
-        res.locals.postsPerPage = atomPostPerPage;
+    module.exports.atom = function (req, res) {
+        when.join(
+            provider.findAll({ limit: atomPostPerPage })
+        )
+        .spread(function (posts) {
 
-        res.type('atom').render(view(req, 'atom'));
-    })
-    .catch(function (err) {
-        res.send(500);
-    });
-};
+            res.locals.posts        = posts;
+            res.locals.updated      = maxUpdatedDate(posts);
+            res.locals.startIndex   = 0;
+            res.locals.postsPerPage = atomPostPerPage;
 
-function maxUpdatedDate (posts) {
-    var dates = posts.map(function (post) {
-        return post.publishedAt;
-    }), max = Math.max.apply(null, dates);
+            res.type('atom').render(view(req, 'atom'));
+        })
+        .catch(function (err) {
+            res.send(500);
+        });
+    };
 
-    return new Date(max);
-}
+    function maxUpdatedDate(posts) {
+        var dates = posts.map(function (post) {
+            return post.publishedAt;
+        }), max = Math.max.apply(null, dates);
+
+        return new Date(max);
+    }
+
+})();
