73fd1e18c415b4ddb4755a954f9cede8bf4a3b21
Tom <pawlaktom@gmail.com>
2014-04-15 12:35:18 +0100

    String check added

--- core/server/init.js
+++ core/server/init.js
@@ -1,12 +1,14 @@
 (function () {
     "use strict";
 
-    var when    = require('when'),
-        _       = require('lodash');
+    var when = require('when'),
+        _ = require('lodash');
 
     module.exports = function () {
         _.mixin({ 'startsWith': startsWith }, { chain: false });
         _.mixin({ 'endsWith': endsWith }, { chain: false });
+        _.mixin({ 'isEmpty': isEmpty }, { chain: false });
+        _.mixin({ 'isBlank': isBlank }, { chain: false });
 
         return when.resolve();
     };
@@ -18,6 +20,14 @@
 
     function endsWith(string, suffix) {
         return string.indexOf(suffix, string.length - suffix.length) !== -1;
+    }
+
+    function isEmpty(string) {
+        return (!string || 0 === string.length);
+    }
+
+    function isBlank(string) {
+        return (!string || 0 === string.length || /^\s*$/.test(string));
     }
 
 }());
