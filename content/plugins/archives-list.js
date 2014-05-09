f5083f3dbc38698a8853781a06366c4dde422a5c
Tom <pawlaktom@gmail.com>
2014-05-09 10:14:01 +0100

    Plugin updates

--- content/plugins/archives-list.js
+++ content/plugins/archives-list.js
@@ -1,19 +1,21 @@
 (function () {
     "use strict";
 
-    var when = require('when'), provider;
+    var when     = require('when'),
+        provider = require('../../core/server/providers').postProvider;
+
+    module.exports = {
+        name: "Archive List Plugin",
+        description: "Generates archives list from the live posts and assigns it to the 'archive' template variable",
+        author: "Panda",
+
+        request: function (req, res) {
+            return provider.archiveInfo().then(function (archive) {
+                res.locals.archives = archive;
+            });
+        }
+    };
 
     require('pkginfo')(module, 'version');
 
-    module.exports.name = "Archive List Plugin";
-    module.exports.description = "Generates archives list from the live posts and assigns it to the 'archive' template variable.";
-    module.exports.author = "Panda";
-
-    module.exports.start = function () {
-        provider = require('../../core/server/providers').postProvider;
-    };
-
-    module.exports.request = function (req, res) {
-        return provider.archiveInfo().then(function (archive) { res.locals.archives = archive; });
-    };
 }());
--- content/plugins/labels-list.js
+++ content/plugins/labels-list.js
@@ -1,20 +1,21 @@
 (function () {
     "use strict";
 
-    var when = require('when'), provider;
+    var when     = require('when'),
+        provider = require('../../core/server/providers').postProvider;
+
+    module.exports = {
+        name: "Labels List Plugin",
+        description: "Generates labels list from the live posts and assigns it to the 'labels' template variable",
+        author: "Panda",
+
+        request: function (req, res) {
+            return provider.labelsInfo({ live: true }).then(function (labels) {
+                res.locals.labels = labels;
+            });
+        }
+    };
 
     require('pkginfo')(module, 'version');
-
-    module.exports.name = "Labels List Plugin";
-    module.exports.description = "Generates labels list from the live posts and assigns it to the 'labels' template variable.";
-    module.exports.author = "Panda";
-
-    module.exports.start = function () {
-        provider = require('../../core/server/providers').postProvider;
-    };
-
-    module.exports.request = function (req, res) {
-        return provider.labelsInfo({ live:true }).then(function (labels) { res.locals.labels = labels; });
-    };
 
 }());
