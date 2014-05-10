3280d2d9723bfb1d7907e384788c10d267a5ff95
Tom Pawlak <tompwk@gmail.com>
2014-05-10 16:30:10 +0100

    Image upload functionality added

--- core/server/controllers/api/fileupload.js
+++ core/server/controllers/api/fileupload.js
@@ -1,15 +1,33 @@
 (function () {
     "use strict";
 
-    var multiparty  = require('multiparty');
+    var multiparty  = require('multiparty'),
+        provider    = require('../../providers').imageProvider;
 
     module.exports = function (req, res) {
+        console.info('file upload request ...')
+
         var form = new multiparty.Form();
 
-        form.parse(req, function (err, fields, files) {
-            console.info(require('util').inspect({fields: fields, files: files}, {depth: 5}));
-            res.send(200);
+        form.on('part', function(part){
+            if (!part.filename) return;
+
+            var size = part.byteCount - part.byteOffset,
+                name = part.filename;
+
+            console.info('Uploading file to azure blob : ', name, size);
+
+            provider.createFromStream(name, size, part);
         });
+
+
+        form.parse(req);
+        res.send('File uploaded successfully');
+//
+//        form.parse(req, function (err, fields, files) {
+//            console.info(require('util').inspect({fields: fields, files: files}, {depth: 5}));
+//            res.send(200);
+//        });
     };
 
 }());
