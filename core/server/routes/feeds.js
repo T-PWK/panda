cc14e854ecbb1a00bae212c1ecf29bed0fca7546
Tom <pawlaktom@gmail.com>
2014-05-14 12:44:02 +0100

    RSS and Atom feeds moved to plugins

--- core/server/routes/feeds.js
+++ core/server/routes/feeds.js
@@ -2,14 +2,10 @@
     'use strict';
 
     var ips         = require('./../middleware/ips'),
-        feeds       = require('./../controllers/feeds'),
         robots      = require('../controllers/robots');
 
     module.exports = function (app) {
-//        app.get('/sitemap.xml', ips.siteIpCheck, feeds.sitemap);
         app.get('/robots.txt', ips.siteIpCheck, robots);
-//        app.get('/rss', ips.siteIpCheck, feeds.middleware, feeds.rss);
-//        app.get('/atom', ips.siteIpCheck, feeds.middleware, feeds.atom);
     };
 
 })();
