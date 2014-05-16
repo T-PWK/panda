438cc125f3b285f4e62e0cde8a6bb2de5574e305
Tom <pawlaktom@gmail.com>
2014-05-16 10:28:33 +0100

    Request handlers update
    All feed handlers, including robots.txt and sitemap.xml moved to plugins

--- core/server/routes/index.js
+++ core/server/routes/index.js
@@ -2,13 +2,11 @@
     'use strict';
 
     var frontend = require('./frontend'),
-        feeds = require('./feeds'),
         admin = require('./admin'),
         api = require('./api');
 
     module.exports = {
         frontend: frontend,
-        feeds: feeds,
         admin: admin,
         api: api
     };
